fbbce854f7f784499ea2c656396fb4f20325e900
Dani-r-36 <danishanr0@gmail.com>
2024-04-19 10:18:57 +0100

    http done

--- projects/http-auth/main.go
+++ projects/http-auth/main.go
@@ -6,9 +6,11 @@ import (
 	"crypto/subtle"
 	"fmt"
 	"html"
-	"io/ioutil"
 	"net/http"
 	"os"
+	"golang.org/x/time/rate"
+	"io"
+	"strings"
 )
 
 var htmlStart string = `
@@ -20,76 +22,87 @@ var queryHtml string = `
 <ul>
 <li>foo: `
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	foo := queryParams.Get("foo")
-	switch r.Method {
-		case http.MethodGet:
     // tmpl, err := template.ParseFiles("template/index.html")
     // if err != nil {
     //   http.Error(w, err.Error(), http.StatusInternalServerError)
     //   return
     // }
 	// tmpl.Execute(w, nil)
-			w.Header().Set("Content-Type", "text/html")
-			escaped := html.EscapeString(foo)
-    		response := htmlStart + greeting + queryHtml + escaped +"</li>\n</ul>"
-			w.Write([]byte(response))
-  		case http.MethodPost:
-    		body, _ := ioutil.ReadAll(r.Body)
-			defer r.Body.Close()
-			w.Header().Set("Content-Type", "text/plain")
-			escaped := html.EscapeString(string(body))
-			response := "<!DOCTYPE html><html>" + escaped + "</html>"
-			w.Write([]byte(response))
-  		default:
-    		fmt.Fprintf(w, "Unsupported method: %s", r.Method)
-  	}
-}
 
-func protectedHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "This is the protected handler")
-}
 
-func authenticated(w http.ResponseWriter, r *http.Request) {
-	username, password, ok := r.BasicAuth()
-	if ok {
-		// Calculate SHA-256 hashes for the provided and expected
-		// usernames and passwords.
-		usernameHash := sha256.Sum256([]byte(username))
-		passwordHash := sha256.Sum256([]byte(password))
-		expectedUsernameHash := sha256.Sum256([]byte(os.Getenv("AUTH_USERNAME")))
-		expectedPasswordHash := sha256.Sum256([]byte(os.Getenv("AUTH_PASSWORD")))
 
-		// Use the subtle.ConstantTimeCompare() function to check if 
-		// the provided username and password hashes equal the  
-		// expected username and password hashes. ConstantTimeCompare
-		// will return 1 if the values are equal, or 0 otherwise. 
-		// Importantly, we should to do the work to evaluate both the 
-		// username and password before checking the return values to 
-		// avoid leaking information.
-		usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
-		passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
-
-		// If the username and password are correct, then call
-		// the next handler in the chain. Make sure to return 
-		// afterwards, so that none of the code below is run.
-		if usernameMatch && passwordMatch {
-			response := htmlStart + "Hello " + username
-			w.Write([]byte(response))
-			return
+func rateLimit(limiter *rate.Limiter, next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if limiter.Allow() {
+			next.ServeHTTP(w, r)
+		} else {
+			http.Error(w, "Service Unavailable", http.StatusServiceUnavailable)
 		}
-	}
-
-	// If the Authentication header is not present, is invalid, or the
-	// username or password is wrong, then set a WWW-Authenticate 
-	// header to inform the client that we expect them to use basic
-	// authentication and send a 401 Unauthorized response.
-	w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
-	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
 }
+
 func main() {
-	http.HandleFunc("/", handleRequest)
-	http.HandleFunc("/authenticated", authenticated)
+	http.HandleFunc("/",func(w http.ResponseWriter, r *http.Request){
+		queryParams := r.URL.Query()
+		foo := queryParams.Get("foo")
+		switch r.Method {
+			case http.MethodGet:
+				w.Header().Set("Content-Type", "text/html")
+				escaped := html.EscapeString(foo)
+				response := htmlStart + greeting + queryHtml + escaped +"</li>\n</ul>"
+				w.Write([]byte(response))
+			case http.MethodPost:
+				body := new(strings.Builder)
+			if _, err := io.Copy(body, r.Body); err != nil {
+				fmt.Printf("Error getting body: %v", err)
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write([]byte("Internal server error"))
+				return
+			}
+			response := htmlStart + greeting +body.String()
+			w.Write([]byte(html.EscapeString(response)))
+			default:
+				fmt.Fprintf(w, "Unsupported method: %s", r.Method)
+			}
+		})
+		
+	http.HandleFunc("/200", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("200"))
+	})
+	http.HandleFunc("/authenticated", func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if ok {
+			// Calculate SHA-256 hashes for the provided and expected
+			// usernames and passwords.
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(os.Getenv("AUTH_USERNAME")))
+			expectedPasswordHash := sha256.Sum256([]byte(os.Getenv("AUTH_PASSWORD")))
+
+			// Use the subtle.ConstantTimeCompare() function to check if 
+			// the provided username and password hashes equal the  
+			// expected username and password hashes.
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+			if usernameMatch && passwordMatch {
+				response := htmlStart + "Hello " + username
+				w.Write([]byte(response))
+				return
+			}
+		}
+
+		// If the Authentication header is not present, is invalid, or the
+		// username or password is wrong,
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		})
+
+	limiter := rate.NewLimiter(100, 30)
+	http.HandleFunc("/limited", rateLimit(limiter, func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		w.Write([]byte("<!DOCTYPE html>\n<html>\nHello world!"))
+	}))
+
 	http.ListenAndServe(":8080", nil)
 }
